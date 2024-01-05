dc599f6c6dda94e257c93ddda8c9cfe53c7072f4
Bram <bramleisink@outlook.com>
2024-01-05 22:58:01 +0100

    Load marks form json file, and display them

--- src/routes/cijfers/+page.svelte
+++ src/routes/cijfers/+page.svelte
@@ -1,3 +1,48 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let marks = [];
+	let marksGroupedBySubject = [];
+	let marksGroupedByExamWeek = [];
+
+	// Function to fetch data from marks.json
+	const fetchMarks = async () => {
+		const response = await fetch('src/routes/cijfers/marks.json');
+		marks = await response.json();
+
+		marksGroupedBySubject = await marks.reduce((acc, obj) => {
+			const subject = obj.subject.name;
+
+			if (!acc[subject]) {
+				acc[subject] = [];
+			}
+
+			acc[subject].push(obj);
+
+			return acc;
+		}, {});
+
+		marksGroupedByExamWeek = await marks.reduce((acc, obj) => {
+			const semester = obj.conduct.semester;
+			const inExamWeek = obj.conduct.inExamWeek;
+
+			if (inExamWeek) {
+				if (!acc[semester]) {
+					acc[semester] = [];
+				}
+
+				acc[semester].push(obj);
+			}
+			return acc;
+		}, {});
+	};
+
+	// Fetch marks when the component is mounted
+	onMount(() => {
+		fetchMarks();
+	});
+</script>
+
 <div class="stats stats-vertical sm:stats-horizontal shadow">
 	<div class="stat">
 		<div class="stat-title">Toetsen gemaakt</div>
@@ -26,7 +71,6 @@
 		<h3 class="text-xl font-bold">Recent toegevoegd</h3>
 		<div class="overflow-x-auto">
 			<table class="table">
-				<!-- head -->
 				<thead>
 					<tr>
 						<th>Naam</th>
@@ -36,36 +80,23 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Levensloop deel 2
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Economie</td>
-						<td class="text-success">7,0</td>
-						<td>2</td>
-					</tr>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Leesvaardigheid
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Engels</td>
-						<td class="text-success">7,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Bewegingen beschrijven</td>
-						<td>Natuurkunde</td>
-						<td class="text-success">8,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Levensloop deel 1</td>
-						<td>Economie</td>
-						<td class="text-error">5,0</td>
-						<td>1</td>
-					</tr>
+					{#each marks as mark}
+						<tr class="hover">
+							<td
+								><b>{mark.id}</b> - {mark.name}
+								{#if mark.result.grades[mark.result.grades.length - 1].new}
+									<div class="badge badge-primary ml-2">nieuw</div>
+								{/if}</td
+							>
+							<td class="capitalize">{mark.subject.name}</td>
+							<td
+								class="text-{mark.result.grades[mark.result.grades.length - 1].pass
+									? 'success'
+									: 'error'}">{mark.result.grades[mark.result.grades.length - 1].grade}</td
+							>
+							<td>{mark.weight}</td>
+						</tr>
+					{/each}
 				</tbody>
 			</table>
 		</div>
@@ -73,198 +104,77 @@
 
 	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Vakken" />
 	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-		<h3 class="text-xl font-bold">Natuurkunde</h3>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th>Naam</th>
-						<th>Vak</th>
-						<th>Cijfer</th>
-						<th>Weging</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Levensloop deel 2
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Economie</td>
-						<td class="text-success">7,0</td>
-						<td>2</td>
-					</tr>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Leesvaardigheid
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Engels</td>
-						<td class="text-success">7,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Bewegingen beschrijven</td>
-						<td>Natuurkunde</td>
-						<td class="text-success">8,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Levensloop deel 1</td>
-						<td>Economie</td>
-						<td class="text-error">5,0</td>
-						<td>1</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+		{#each Object.keys(marksGroupedBySubject) as subject}
+			<h3 class="text-xl font-bold capitalize">{subject}</h3>
+			<div class="overflow-x-auto mb-3">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Naam</th>
+							<th>Datum</th>
+							<th>Cijfer</th>
+							<th>Weging</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each marksGroupedBySubject[subject] as mark}
+							<tr class="hover">
+								<td
+									><b>{mark.id}</b> - {mark.name}
+									{#if mark.result.grades[mark.result.grades.length - 1].new}
+										<div class="badge badge-primary ml-2">nieuw</div>
+									{/if}</td
+								>
+								<td class="capitalize">{mark.conduct.date}</td>
+								<td
+									class="text-{mark.result.grades[mark.result.grades.length - 1].pass
+										? 'success'
+										: 'error'}">{mark.result.grades[mark.result.grades.length - 1].grade}</td
+								>
+								<td>{mark.weight}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		{/each}
 	</div>
 
 	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Toetsweken" />
 	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-		<h3 class="text-xl font-bold">Toetsweek 1</h3>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th>Naam</th>
-						<th>Vak</th>
-						<th>Cijfer</th>
-						<th>Weging</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Levensloop deel 2
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Economie</td>
-						<td class="text-success">7,0</td>
-						<td>2</td>
-					</tr>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Leesvaardigheid
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Engels</td>
-						<td class="text-success">7,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Bewegingen beschrijven</td>
-						<td>Natuurkunde</td>
-						<td class="text-success">8,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Levensloop deel 1</td>
-						<td>Economie</td>
-						<td class="text-error">5,0</td>
-						<td>1</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-
-	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Aankomend" />
-	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-		<h3 class="text-xl font-bold">Datum bekend</h3>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th>Naam</th>
-						<th>Vak</th>
-						<th>Datum</th>
-						<th>Weging</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Levensloop deel 2
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Economie</td>
-						<td class="text-success">7,0</td>
-						<td>2</td>
-					</tr>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Leesvaardigheid
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Engels</td>
-						<td class="text-success">7,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Bewegingen beschrijven</td>
-						<td>Natuurkunde</td>
-						<td class="text-success">8,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Levensloop deel 1</td>
-						<td>Economie</td>
-						<td class="text-error">5,0</td>
-						<td>1</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-
-		<h3 class="text-xl font-bold">Deze periode</h3>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th>Naam</th>
-						<th>Vak</th>
-						<th>Datum</th>
-						<th>Weging</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Levensloop deel 2
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Economie</td>
-						<td class="text-success">7,0</td>
-						<td>2</td>
-					</tr>
-					<tr class="hover">
-						<td
-							><b>T402</b> - Leesvaardigheid
-							<div class="badge badge-primary ml-2">nieuw</div></td
-						>
-						<td>Engels</td>
-						<td class="text-success">7,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Bewegingen beschrijven</td>
-						<td>Natuurkunde</td>
-						<td class="text-success">8,9</td>
-						<td>1</td>
-					</tr>
-					<tr class="hover">
-						<td><b>T401</b> - Levensloop deel 1</td>
-						<td>Economie</td>
-						<td class="text-error">5,0</td>
-						<td>1</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+		{#each Object.keys(marksGroupedByExamWeek) as examWeek}
+			<h3 class="text-xl font-bold capitalize">Toetsweek {examWeek}</h3>
+			<div class="overflow-x-auto mb-3">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Naam</th>
+							<th>Vak</th>
+							<th>Cijfer</th>
+							<th>Weging</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each marksGroupedByExamWeek[examWeek] as mark}
+							<tr class="hover">
+								<td
+									><b>{mark.id}</b> - {mark.name}
+									{#if mark.result.grades[mark.result.grades.length - 1].new}
+										<div class="badge badge-primary ml-2">nieuw</div>
+									{/if}</td
+								>
+								<td class="capitalize">{mark.subject.name}</td>
+								<td
+									class="text-{mark.result.grades[mark.result.grades.length - 1].pass
+										? 'success'
+										: 'error'}">{mark.result.grades[mark.result.grades.length - 1].grade}</td
+								>
+								<td>{mark.weight}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		{/each}
 	</div>
 </div>
