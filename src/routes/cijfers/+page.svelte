922797000682facd35d4d554cdef177bf59ebeea
Bram <bramleisink@outlook.com>
2024-01-05 16:49:17 +0100

    Mockup cijfers tabel (niet responsive)

--- src/routes/cijfers/+page.svelte
+++ src/routes/cijfers/+page.svelte
@@ -1,21 +1,270 @@
-<div class="stats stats-vertical lg:stats-horizontal shadow">
-  
-  <div class="stat">
-    <div class="stat-title">Toetsen gemaakt</div>
-    <div class="stat-value">23</div>
-    <div class="stat-desc">2022 - 2023 (34%)</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-title">Gemiddelde</div>
-    <div class="stat-value">8,24</div>
-    <div class="stat-desc text-success">↗︎ 0,46 (5,5%)</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-title">Voldoendes</div>
-    <div class="stat-value">91,3%</div>
-    <div class="stat-desc"><span class="text-success">21</span> / <span class="text-error">2</span></div>
-  </div>
-  
+<div class="stats stats-vertical sm:stats-horizontal shadow">
+	<div class="stat">
+		<div class="stat-title">Toetsen gemaakt</div>
+		<div class="stat-value">23</div>
+		<div class="stat-desc">2022 - 2023 (34%)</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-title">Gemiddelde</div>
+		<div class="stat-value">8,24</div>
+		<div class="stat-desc text-success">↗︎ 0,46 (5,5%)</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-title">Voldoendes</div>
+		<div class="stat-value">91,3%</div>
+		<div class="stat-desc">
+			<span class="text-success">21</span> / <span class="text-error">2</span>
+		</div>
+	</div>
+</div>
+
+<div role="tablist" class="tabs tabs-lifted pt-5">
+	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Recent" checked />
+	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+		<h3 class="text-xl font-bold">Recent toegevoegd</h3>
+		<div class="overflow-x-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Vak</th>
+						<th>Cijfer</th>
+						<th>Weging</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Levensloop deel 2
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Economie</td>
+						<td class="text-success">7,0</td>
+						<td>2</td>
+					</tr>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Leesvaardigheid
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Engels</td>
+						<td class="text-success">7,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Bewegingen beschrijven</td>
+						<td>Natuurkunde</td>
+						<td class="text-success">8,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Levensloop deel 1</td>
+						<td>Economie</td>
+						<td class="text-error">5,0</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Vakken" />
+	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+		<h3 class="text-xl font-bold">Natuurkunde</h3>
+		<div class="overflow-x-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Vak</th>
+						<th>Cijfer</th>
+						<th>Weging</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Levensloop deel 2
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Economie</td>
+						<td class="text-success">7,0</td>
+						<td>2</td>
+					</tr>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Leesvaardigheid
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Engels</td>
+						<td class="text-success">7,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Bewegingen beschrijven</td>
+						<td>Natuurkunde</td>
+						<td class="text-success">8,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Levensloop deel 1</td>
+						<td>Economie</td>
+						<td class="text-error">5,0</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Toetsweken" />
+	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+		<h3 class="text-xl font-bold">Toetsweek 1</h3>
+		<div class="overflow-x-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Vak</th>
+						<th>Cijfer</th>
+						<th>Weging</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Levensloop deel 2
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Economie</td>
+						<td class="text-success">7,0</td>
+						<td>2</td>
+					</tr>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Leesvaardigheid
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Engels</td>
+						<td class="text-success">7,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Bewegingen beschrijven</td>
+						<td>Natuurkunde</td>
+						<td class="text-success">8,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Levensloop deel 1</td>
+						<td>Economie</td>
+						<td class="text-error">5,0</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Aankomend" />
+	<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+		<h3 class="text-xl font-bold">Datum bekend</h3>
+		<div class="overflow-x-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Vak</th>
+						<th>Datum</th>
+						<th>Weging</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Levensloop deel 2
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Economie</td>
+						<td class="text-success">7,0</td>
+						<td>2</td>
+					</tr>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Leesvaardigheid
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Engels</td>
+						<td class="text-success">7,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Bewegingen beschrijven</td>
+						<td>Natuurkunde</td>
+						<td class="text-success">8,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Levensloop deel 1</td>
+						<td>Economie</td>
+						<td class="text-error">5,0</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<h3 class="text-xl font-bold">Deze periode</h3>
+		<div class="overflow-x-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Vak</th>
+						<th>Datum</th>
+						<th>Weging</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Levensloop deel 2
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Economie</td>
+						<td class="text-success">7,0</td>
+						<td>2</td>
+					</tr>
+					<tr class="hover">
+						<td
+							><b>T402</b> - Leesvaardigheid
+							<div class="badge badge-primary ml-2">nieuw</div></td
+						>
+						<td>Engels</td>
+						<td class="text-success">7,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Bewegingen beschrijven</td>
+						<td>Natuurkunde</td>
+						<td class="text-success">8,9</td>
+						<td>1</td>
+					</tr>
+					<tr class="hover">
+						<td><b>T401</b> - Levensloop deel 1</td>
+						<td>Economie</td>
+						<td class="text-error">5,0</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
