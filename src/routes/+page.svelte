43f28e095fc86cc765028fc55b28c6bba6960dd3
Bram <bramleisink@outlook.com>
2024-01-06 11:44:22 +0100

    Changed url from "cijfers" to "marks"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1 class="text-2xl font-bold">Welkom bij Rooster 🐓</h1>
-<p>Dit wordt jouw dashboard! Bezoek <u><a href="https://bramleisink.github.io/rooster-documentation/">de Github</a></u> voor de documentatie.</p>
+<div class="hero min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-4xl font-bold">Welkom bij Rooster 🐓</h1>
+			<p class="py-6">
+				Dit wordt jouw dashboard! Bezoek <u
+					><a href="https://bramleisink.github.io/rooster-documentation/">de Github</a></u
+				> voor de documentatie.
+			</p>
+			<a href="/marks" class="btn btn-primary">Cijfers bekijken</a>
+		</div>
+	</div>
+</div>
