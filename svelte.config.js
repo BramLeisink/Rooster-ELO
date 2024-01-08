dd53daefe9c97a19383fc9db56df36794c69c309
Bram Leisink <bramleisink@outlook.com>
2024-01-08 14:33:37 +0100

    work please

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/rooster-elo' : '',
+			base: process.env.NODE_ENV === 'production' ? '' : '',
 		}
 	}
 };
