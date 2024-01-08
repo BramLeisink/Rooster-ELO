c0e34df1d42088413e5bd587c2c2f4c1cf9c88e4
Bram <bramleisink@outlook.com>
2024-01-08 16:28:53 +0100

    Dit is echt heel raar allemaal

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
 	}
 };
 
