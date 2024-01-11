3e2c4d0968c302d55029b342a4d5050d8bffadaf
BoKersten <99825528+BoKersten@users.noreply.github.com>
2024-01-11 15:03:35 +0100

    Update +page.svelte

--- src/routes/instellingen/+page.svelte
+++ src/routes/instellingen/+page.svelte
@@ -3,3 +3,12 @@
 <title>instellingen</title>
 
 </head>
+
+<div class="hero min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-4xl font-bold">Instellingen</h1>
+			
+		</div>
+	</div>
+</div>
