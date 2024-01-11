8705e4411d1e37d655b01bf2663f8196eedddac5
BoKersten <99825528+BoKersten@users.noreply.github.com>
2024-01-11 15:26:37 +0100

    Update +page.svelte
    img zelfde als bij navbar en naam enzo toegevoegd

--- src/routes/profiel/+page.svelte
+++ src/routes/profiel/+page.svelte
@@ -4,8 +4,12 @@
 
 </head>
 
-<section class="profile">
+<section class="profile content-center">
 
-<img src=".../static/favicon.png" class="rounded-full"/>
-
+<img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" class="rounded-full w-36 h-36" hover:bg-blue-500 hover:bg-opcaity-10/>
+<h2>{name}</h2>
   </section>
+<script>
+
+let name = "jhon doe";
+</script>
