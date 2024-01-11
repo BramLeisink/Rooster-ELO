e6effc15e7970d54c141c77b926a7e75a2cbcda9
BoKersten <bokersten2008@gmail.com>
2024-01-11 17:21:53 +0100

    profile card added to /profile

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,11 +4,18 @@
 
 </head>
 
-<section class="profile content-center">
 
-<img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" class="rounded-full w-36 h-36" hover:bg-blue-500 hover:bg-opcaity-10/>
-<h2>{name}</h2>
-  </section>
+<div class="card w-96 h-96 bg-base-100 shadow-xl content-center">
+  <div class="card-body items-center text-center">
+<div class="avatar rounded-full hover:opacity-90 hover:cursor-pointer">
+  <div class="w-60 rounded-full">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div>
+<h2 class="card-title pointer-events-none">{name}</h2>
+
+</div>
+ </div>
 <script>
 
 let name = "jhon doe";
